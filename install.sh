802712dde4c60ab1c1a1583efcad902725f9a4c9
MoisesMenaG <menagonzalezmoises@hotmail.com>
2021-04-05 00:00:13 -0600

    Readme added

--- install.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-rsync -arvzP temp/* .
-rsync -arvzP temp/.[^.]* .
-rm -rf temp/
--- wp-content/themes/casumi-consulting/template-components/components/cpt-text.php
+++
@@ -1,3 +0,0 @@
-<div class="custom-cpt text-cpt <?php the_sub_field('style_text'); ?>-text mb-3">
-    <?php the_sub_field('content_text'); ?>
-</div>
